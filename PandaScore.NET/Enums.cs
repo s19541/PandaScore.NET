ece0a88fc3c4fd018d07c5093c63c1ec6bd27271
s19541 <s19541@pjwstk.edu.pl>
2021-09-10 22:53:52 +0200

    Poprawki enumow

--- PandaScore.NET/Enums.cs
+++ PandaScore.NET/Enums.cs
@@ -5,6 +5,6 @@ using System.Text;
 namespace PandaScoreNET
 {
     public enum OpponentType { player, team }
-    public enum MatchStatus { canceled, finished, not_started, running }
+    public enum MatchStatus { canceled, finished, not_started, running, postponed }
     public enum MatchType { best_of, custom, ow_best_of }
 }
